376427edb2861faeb416015587a17d131082bdaf
arangates <arangates@gmail.com>
2016-10-03 18:45:12 +0530

    Added blood pressure and temperature visualization in dashbord (dummy data)

--- README.md
+++ README.md
@@ -4,20 +4,8 @@ A cheap health monitoring system for people who really needs it.
 
 ## Code Example
 
-We are using MongoDB databas (Hosted on MongoLab), HTML5/CSS3/JavaScript in frontend. PHP/Python3 in backend.<br />
+We are using MongoDB database , HTML5/CSS3/JavaScript in frontend. PHP/Python3 in backend.<br />
 Ubuntu 14.04.4 LTS, AWS server is used for backend.
-
-## MongoDB credentials
-URL: ds047666.mlab.com
-User: test<br />
-Pass: test<br />
-shell connection string: mongo ds047666.mlab.com:47666/pdata -u test -p test<br />
-Mongo connection string: mongodb://test:test@ds047666.mlab.com:47666/pdata<br />
-
-## Server credentials
-Public IP: http://52.39.9.186/<br />
-User: ubuntu<br />
-key is used for authentication.
 
 ## Instructions to run code
 1.git clone https://github.com/arangates/smartx-healthcare.git<br />
@@ -26,3 +14,8 @@ key is used for authentication.
 3.bower install<br />
 4.python -m SimpleHTTPServer 8000<br />
 5.view dashboard at localhost:8000/<br />
+
+## Server credentials
+Public IP: http://52.39.9.186/<br />
+User: ubuntu<br />
+key is used for authentication.
--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,94 @@
+// BP realtime data
+
 setInterval(function() {
-		var count = parseFloat(document.getElementsByClassName('count')[0].innerHTML);
-		count = count + 0.1;
-		document.getElementsByClassName('count')[0].innerHTML = count.toFixed(1);
+		var pulse = parseFloat(document.getElementById('pulse').innerHTML);
+		console.log(document.getElementById('pulse').innerHTML);
+		pulse = pulse + 0.1;
+		document.getElementById('pulse').innerHTML = pulse.toFixed(1);
 
 }, 1000)
 
 setInterval(function() {
-		var count = parseFloat(document.getElementsByClassName('small-count')[0].innerHTML);
-		count = count + 0.1;
-		document.getElementsByClassName('small-count')[0].innerHTML = count.toFixed(1);
+		var pulseavg = parseFloat(document.getElementById('pulseavg').innerHTML);
+		pulseavg = pulseavg + 0.1;
+		document.getElementById('pulseavg').innerHTML = pulseavg.toFixed(1);
 
 }, 1500)
 
 setInterval(function() {
-		var count = parseFloat(document.getElementsByClassName('small-count1')[0].innerHTML);
-		count = count + 1.1;
-		document.getElementsByClassName('small-count1')[0].innerHTML = count.toFixed(1);
+		var pulsemax = parseFloat(document.getElementById('pulsemax').innerHTML);
+		pulsemax = pulsemax + 1.1;
+		document.getElementById('pulsemax').innerHTML = pulsemax.toFixed(1);
 
 }, 1000)
 
 setInterval(function() {
-		var count = parseFloat(document.getElementsByClassName('small-count2')[0].innerHTML);
-		count = count + 0.1;
-		document.getElementsByClassName('small-count2')[0].innerHTML = count.toFixed(1);
+		var pulselast = parseFloat(document.getElementById('pulselast').innerHTML);
+		pulselast = pulselast + 0.1;
+		document.getElementById('pulselast').innerHTML = pulselast.toFixed(1);
+
+}, 1000)
+
+
+// BP realtime data
+
+setInterval(function() {
+		var bp = parseFloat(document.getElementById('bp').innerHTML);
+		bp = bp + 0.1;
+		document.getElementById('bp').innerHTML = bp.toFixed(1);
+		console.log(document.getElementById('bp').innerHTML);
+
+}, 1000)
+
+setInterval(function() {
+		var bpavg = parseFloat(document.getElementById('bpavg').innerHTML);
+		bpavg = bpavg + 0.1;
+		document.getElementById('bpavg').innerHTML = bpavg.toFixed(1);
+
+}, 1500)
+
+setInterval(function() {
+		var bpmax = parseFloat(document.getElementById('bpmax').innerHTML);
+		bpmax = bpmax + 1.1;
+		document.getElementById('bpmax').innerHTML = bpmax.toFixed(1);
+
+}, 1000)
+
+setInterval(function() {
+		var bplast = parseFloat(document.getElementById('bplast').innerHTML);
+		bplast = bplast + 0.1;
+		document.getElementById('bplast').innerHTML = bplast.toFixed(1);
+
+}, 1000)
+
+
+
+// Temperature realtime data
+
+setInterval(function() {
+		var temperature = parseFloat(document.getElementById('temperature').innerHTML);
+		temperature = temperature + 0.1;
+		document.getElementById('temperature').innerHTML = temperature.toFixed(1);
+
+}, 1000)
+
+setInterval(function() {
+		var temperatureavg = parseFloat(document.getElementById('temperatureavg').innerHTML);
+		temperatureavg = temperatureavg + 0.1;
+		document.getElementById('temperatureavg').innerHTML = temperatureavg.toFixed(1);
+
+}, 1500)
+
+setInterval(function() {
+		var temperaturemax = parseFloat(document.getElementById('temperaturemax').innerHTML);
+		temperaturemax = temperaturemax + 1.1;
+		document.getElementById('temperaturemax').innerHTML = temperaturemax.toFixed(1);
+
+}, 1000)
+
+setInterval(function() {
+		var temperaturelast = parseFloat(document.getElementById('temperaturelast').innerHTML);
+		temperaturelast = temperaturelast + 0.1;
+		document.getElementById('temperaturelast').innerHTML = temperaturelast.toFixed(1);
 
 }, 1000)
