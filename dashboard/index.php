4616b7e0f9ab7cdec1f752d3bdf5ab731a9c59a1
deepak125 <deepak125.dk@gmail.com>
2016-10-04 05:13:58 +0000

    splitted files into header and footer

---
+++ dashboard/index.php
@@ -0,0 +1,31 @@
+<?php include './header.php'; ?>
+<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">            
+    <ul class="nav menu">
+        <li class="active"><a href="index.html"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Dashboard</a></li>
+        <li><a href="pulse.html"><svg class="glyph stroked calendar"><use xlink:href="#stroked-calendar"></use></svg> Pulse</a></li>
+        <li><a href="bp.html"><svg class="glyph stroked line-graph"><use xlink:href="#stroked-line-graph"></use></svg> Blood Pressure</a></li>
+        <li><a href="temp.html"><svg class="glyph stroked table"><use xlink:href="#stroked-table"></use></svg> Temperature</a></li>
+        <li><a href="report.html"><svg class="glyph stroked pencil"><use xlink:href="#stroked-pencil"></use></svg> Report </a></li>
+        <li><a href="cunsult.html"><svg class="glyph stroked app-window"><use xlink:href="#stroked-app-window"></use></svg> Consult</a></li>                
+        <li role="presentation" class="divider"></li>                
+    </ul>
+</div><!--/.sidebar-->
+
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+    <div class="row">
+        <ol class="breadcrumb">
+            <li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+            <li class="active">Icons</li>
+        </ol>
+    </div><!--/.row-->
+
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Dashboard</h1>
+        </div>
+    </div><!--/.row-->
+
+
+</div>	<!--/.main-->
+
+<?php include './footer.php'; ?>
