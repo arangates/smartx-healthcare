3274657033c9c739b703ad7c060d11f0fb480b3e
arangates <arangates@gmail.com>
2016-10-04 06:14:35 +0530

    Added food suggestions sections ,realtime data from amazon server

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,11 @@
-var f1,f2,f3;
+ var f1,f2,f3,cdate;
 var pubnub = PUBNUB.init({
     publish_key: 'pub-c-e9228c83-be49-4efa-85b9-3396edf738db',
     subscribe_key: 'sub-c-ba81fc1e-896e-11e6-974e-0619f8945a4f'
 });
 var channel = "Kcteee";
 
-var pulselast= date();
-var bplast = date();
-var temperaturelast = date();
+
 // Pulse
 eon.chart({
     channel: channel,
@@ -45,7 +43,7 @@ eon.chart({
     generate: {
         bindto: '#bprealtime',
         data: {
-            labels: true,
+            labels: true
 
         }
     },
@@ -67,7 +65,7 @@ eon.chart({
     generate: {
         bindto: '#temperaturerealtime',
         data: {
-            type: 'gauge'
+            labels: true
         },
         gauge: {
             min: 0,
@@ -89,54 +87,57 @@ eon.chart({
     }
 });
 (function() {
-    var sensors = "http://api.thingspeak.com/channels/18675/feed/last.json";
+    var sensors = "http://http://52.39.9.186:3000/api/feed/last";
+         setInterval(function() {
+
     $.getJSON(sensors, {
             format: "json"
         })
         .done(function(data) {
-            setInterval(function() {
+           
                 f1 = data.field1; //realtime pulse
                 f2 = data.field2; //realtime bp
-                f3 = data.field3; //realtime temperature
-                // var f4 = data.field4;
-                // var f5 = data.field5;
-                // var f6 = data.field6;
-                // var f7 = data.field7;
-                // var f8=data.field8;
+                f3 = data.field3;
+                cdate = data.created_at; 
                 console.log(f1, f2, f3);
                 pubnub.publish({
                     channel: channel,
                     message: [f1, f2, f3]
                 });
-            }, 1500);
+            
         });
+            }, 1500);
 })();
 
 // BP realtime data
 
 setInterval(function() {
-		var pulse = parseFloat(f1);
-		document.getElementById('pulse').innerHTML = pulse.toFixed(1);
+    var pulse = parseFloat(f1);
+    document.getElementById('pulse').innerHTML = pulse.toFixed(1);
 
 }, 1000)
 
 setInterval(function() {
-		var pulseavg = parseFloat(document.getElementById('pulseavg').innerHTML);
-			document.getElementById('pulseavg').innerHTML = pulseavg.toFixed(1);
+    var pulseavg = parseFloat(document.getElementById('pulseavg').innerHTML);
+      document.getElementById('pulseavg').innerHTML = pulseavg.toFixed(1);
 
 }, 1500)
 
 setInterval(function() {
-		var pulsemax = parseFloat(document.getElementById('pulsemax').innerHTML);
-		
-		document.getElementById('pulsemax').innerHTML = pulsemax.toFixed(1);
+    var pulsemax = parseFloat(document.getElementById('pulsemax').innerHTML);
+    
+    document.getElementById('pulsemax').innerHTML = pulsemax.toFixed(1);
 
 }, 1000)
 
 setInterval(function() {
-		pulselast = ;
-		
-		document.getElementById('pulselast').innerHTML = pulselast;
+    // pulselast = ;
+  
+  cdate=cdate.replace(/(T|Z)/g,' ');
+  console.log(cdate);
+  cdate=cdate.split(' ');
+
+    document.getElementById('pulselast').innerHTML = cdate[1]+'<br>'+cdate[0];
 
 }, 1000)
 
@@ -144,29 +145,29 @@ setInterval(function() {
 // BP realtime data
 
 setInterval(function() {
-		var bp = parseFloat(f2);
-		document.getElementById('bp').innerHTML = bp.toFixed(1);
-		console.log(document.getElementById('bp').innerHTML);
+    var bp = parseFloat(f2);
+    document.getElementById('bp').innerHTML = bp.toFixed(1);
+    console.log(document.getElementById('bp').innerHTML);
 
 }, 1000)
 
 setInterval(function() {
-		var bpavg = parseFloat(document.getElementById('bpavg').innerHTML);
-		
-		document.getElementById('bpavg').innerHTML = bpavg.toFixed(1);
+    var bpavg = parseFloat(document.getElementById('bpavg').innerHTML);
+    
+    document.getElementById('bpavg').innerHTML = bpavg.toFixed(1);
 
 }, 1500)
 
 setInterval(function() {
-		var bpmax = parseFloat(document.getElementById('bpmax').innerHTML);
-		
-		document.getElementById('bpmax').innerHTML = bpmax.toFixed(1);
+    var bpmax = parseFloat(document.getElementById('bpmax').innerHTML);
+    
+    document.getElementById('bpmax').innerHTML = bpmax.toFixed(1);
 
 }, 1000)
 
 setInterval(function() {
-		bplast = parseFloat(document.getElementById('bplast').innerHTML);
-			document.getElementById('bplast').innerHTML = bplast;
+    // bplast = parseFloat(document.getElementById('bplast').innerHTML);
+      document.getElementById('bplast').innerHTML = cdate[1]+'<br>'+cdate[0];
 
 }, 1000)
 
@@ -175,26 +176,26 @@ setInterval(function() {
 // Temperature realtime data
 
 setInterval(function() {
-		var temperature = parseFloat(f3);
-		document.getElementById('temperature').innerHTML = temperature.toFixed(1);
+    var temperature = parseFloat(f3);
+    document.getElementById('temperature').innerHTML = temperature.toFixed(1);
 
 }, 1000)
 
 setInterval(function() {
-		var temperatureavg = parseFloat(document.getElementById('temperatureavg').innerHTML);
-		document.getElementById('temperatureavg').innerHTML = temperatureavg.toFixed(1);
+    var temperatureavg = parseFloat(document.getElementById('temperatureavg').innerHTML);
+    document.getElementById('temperatureavg').innerHTML = temperatureavg.toFixed(1);
 
 }, 1500)
 
 setInterval(function() {
-		var temperaturemax = parseFloat(document.getElementById('temperaturemax').innerHTML);
-		document.getElementById('temperaturemax').innerHTML = temperaturemax.toFixed(1);
+    var temperaturemax = parseFloat(document.getElementById('temperaturemax').innerHTML);
+    document.getElementById('temperaturemax').innerHTML = temperaturemax.toFixed(1);
 
 }, 1000)
 
 setInterval(function() {
-		temperaturelast = parseFloat(document.getElementById('temperaturelast').innerHTML);
-		document.getElementById('temperaturelast').innerHTML = temperaturelast;
+    // temperaturelast = parseFloat(document.getElementById('temperaturelast').innerHTML);
+    document.getElementById('temperaturelast').innerHTML = cdate[1]+'<br>'+cdate[0];
 
 }, 1000)
 
