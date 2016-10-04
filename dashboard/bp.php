313df39016fb4816cf15e04fe78a657040664037
deepak125 <deepak125.dk@gmail.com>
2016-10-04 08:59:08 +0000

    Added Bp and puls graph

---
+++ dashboard/bp.php
@@ -0,0 +1,71 @@
+<?php include './header.php'; ?>
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">	
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Blood Pressure</h1>
+        </div>
+    </div><!--/.row-->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">mmHg / Time(H:M:S)</div>
+                <div class="panel-body">
+                    <div class="canvas-wrapper">
+                        <canvas class="main-chart" id="line-chart" height="200" width="600"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!--/.row-->
+</div>
+<?php include './footer.php'; ?>
+<script>
+    var timestamp_array = [];
+    var dp_array = [];
+    var sp_array = [];
+    $(document).ready(getAllData);
+    function getAllData() {
+        $.ajax({url: "http://52.39.9.186:3000/api/feed/latest/10001/10", success: function (result) {
+                $.each(result, function (index, value) {
+                    var date_temp = new Date(value.time * 1000);
+                    var hours = date_temp.getHours();
+                    var minutes = "0" + date_temp.getMinutes();
+                    var seconds = "0" + date_temp.getSeconds();
+                    var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+                    timestamp_array.push(formattedTime);
+                    dp_array.push(value.diastolic_pressure);
+                    sp_array.push(value.systolic_pressure);
+                });
+                var lineChartData = {
+                    labels: timestamp_array,
+                    datasets : [
+				{
+					label: "Diastolic Pressure",
+					fillColor : "rgba(220,220,220,0.2)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(220,220,220,1)",
+					data : dp_array
+				},
+				{
+					label: "Systolic Ppressure",
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 1)",
+					pointColor : "rgba(48, 164, 255, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(48, 164, 255, 1)",
+					data : sp_array
+				}
+			]
+
+                }
+                var chart1 = document.getElementById("line-chart").getContext("2d");
+                window.myLine = new Chart(chart1).Line(lineChartData, {
+                    responsive: true
+                });
+            }});
+    }
+</script>
--- dashboard/pulse.php
+++ dashboard/pulse.php
@@ -8,7 +8,7 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Line Chart</div>
+                <div class="panel-heading">Pulse rate / Time(H:M:S)</div>
                 <div class="panel-body">
                     <div class="canvas-wrapper">
                         <canvas class="main-chart" id="line-chart" height="200" width="600"></canvas>
@@ -20,45 +20,40 @@
 </div>
 <?php include './footer.php'; ?>
 <script>
+    var timestamp_array = [];
+    var pulse_array = [];
     $(document).ready(getAllData);
     function getAllData() {
-        $.ajax({url: "http://52.39.9.186:3000/api/feeds/", success: function (result) {
-                console.log(result);
+        $.ajax({url: "http://52.39.9.186:3000/api/feed/latest/10001/10", success: function (result) {
+                $.each(result, function (index, value) {
+                    var date_temp = new Date(value.time * 1000);
+                    var hours = date_temp.getHours();
+                    var minutes = "0" + date_temp.getMinutes();
+                    var seconds = "0" + date_temp.getSeconds();
+                    var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+                    timestamp_array.push(formattedTime);
+                    pulse_array.push(value.pulse);
+                });
+                var lineChartData = {
+                    labels: timestamp_array,
+                    datasets: [
+                        {
+                            label: "Last 10 Pulse data",
+                            fillColor: "rgba(48, 164, 255, 0.2)",
+                            strokeColor: "rgba(48, 164, 255, 1)",
+                            pointColor: "rgba(48, 164, 255, 1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(220,220,220,1)",
+                            data: pulse_array
+                        },
+                    ]
+
+                }
+                var chart1 = document.getElementById("line-chart").getContext("2d");
+                window.myLine = new Chart(chart1).Line(lineChartData, {
+                    responsive: true
+                });
             }});
     }
-    var randomScalingFactor = function () {
-        return Math.round(Math.random() * 100)
-    };
-    var lineChartData = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-            },
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(48, 164, 255, 0.2)",
-                strokeColor: "rgba(48, 164, 255, 1)",
-                pointColor: "rgba(48, 164, 255, 1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(48, 164, 255, 1)",
-                data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-            }
-        ]
-
-    }
-    $(document).ready(function () {
-        var chart1 = document.getElementById("line-chart").getContext("2d");
-        window.myLine = new Chart(chart1).Line(lineChartData, {
-            responsive: true
-        });
-    });
 </script>
